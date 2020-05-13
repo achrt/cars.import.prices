63c429b62f1a7e367ad5551f3f9f9cf44abd2e52
achrt <ach@kodix.ru>
2020-05-13 17:15:24 +0300

    [f] fill price codes

--- helpers/color_map.go
+++ helpers/color_map.go
@@ -1,7 +1,7 @@
 package helpers
 
 var ColorPrices = map[string]map[string]map[string]string{
-	"5dce6a50b7f58d0001bd731b" : {
+	"5dce6a50b7f58d0001bd731b" : { // model murano
 		"5ce7dad6d999da000129db3e" : {
 			"price" : "0",
 			"code" : "RBG",
@@ -12,148 +12,148 @@ var ColorPrices = map[string]map[string]map[string]string{
 		},
 		"5ce7dad5d999da000129db16" : {
 			"price" : "0",
-			"code" : "",
+			"code" : "KAD",
 		},
 		"5ce7dad5d999da000129db14" : {
 			"price" : "0",
-			"code" : "",
+			"code" : "K23",
 		},
 		"5ce7dad5d999da000129db0c" : {
 			"price" : "0",
-			"code" : "",
+			"code" : "G41",
 		},
 
 		"5ce7dad6d999da000129db32" : {
 			"price" : "0",
-			"code" : "",
+			"code" : "EAW",
 		},
 		"5ce7dad6d999da000129db2d" : {
 			"price" : "0",
-			"code" : "",
+			"code" : "CAM",
 		},
 	},
-	"5dce6a50b7f58d0001bd732f" : {
+	"5dce6a50b7f58d0001bd732f" : { // model Terrano
 		"5ce7dad6d999da000129db46" : {
 			"price" : "0",
-			"code" : "",
+			"code" : "ZY2",
 		},
 		"5ce7dad6d999da000129db45" : {
 			"price" : "15000",
-			"code" : "",
+			"code" : "ZBD",
 		},
 		"5ce7dad6d999da000129db44" : {
 			"price" : "15000",
-			"code" : "",
+			"code" : "Z11",
 		},
 		"5ce7dad5d999da000129db16" : {
 			"price" : "15000",
-			"code" : "",
+			"code" : "KAD",
 		},
 		"5ce7dad6d999da000129db2f" : {
 			"price" : "15000",
-			"code" : "",
+			"code" : "D17",
 		},
 	},
-	"5dce6a50b7f58d0001bd7358" : {
+	"5dce6a50b7f58d0001bd7358" : { // model Qashqai
 		"5ce7dad6d999da000129db40" : {
 			"price" : "20000",
-			"code" : "",
+			"code" : "RCA",
 		},
 		"5ce7dad6d999da000129db27" : {
 			"price" : "17000",
-			"code" : "",
+			"code" : "RBN",
 		},
 		"5ce7dad5d999da000129db25" : {
 			"price" : "0",
-			"code" : "",
+			"code" : "QM1",
 		},
 		"5ce7dad5d999da000129db22" : {
 			"price" : "20000",
-			"code" : "",
+			"code" : "QAB",
 		},
 		"5ce7dad5d999da000129db1c" : {
 			"price" : "17000",
-			"code" : "",
+			"code" : "NAH",
 		},
 		"5ce7dad5d999da000129db16" : {
 			"price" : "17000",
-			"code" : "",
+			"code" : "KAD",
 		},
 		"5ce7dad5d999da000129db14" : {
 			"price" : "17000",
-			"code" : "",
+			"code" : "K23",
 		},
 		"5ce7dad5d999da000129db0c" : {
 			"price" : "17000",
-			"code" : "",
+			"code" : "G41",
 		},
 	},
-	"5dce6a50b7f58d0001bd7381" : {
+	"5dce6a50b7f58d0001bd7381" : { // model Qashqai
 		"5ce7dad6d999da000129db40" : {
 			"price" : "20000",
-			"code" : "",
+			"code" : "RCA",
 		},
 		"5ce7dad6d999da000129db27" : {
 			"price" : "17000",
-			"code" : "",
+			"code" : "RBN",
 		},
 		"5ce7dad5d999da000129db25" : {
 			"price" : "0",
-			"code" : "",
+			"code" : "QM1",
 		},
 		"5ce7dad5d999da000129db22" : {
 			"price" : "20000",
-			"code" : "",
+			"code" : "QAB",
 		},
 		"5ce7dad5d999da000129db1c" : {
 			"price" : "17000",
-			"code" : "",
+			"code" : "NAH",
 		},
 		"5ce7dad5d999da000129db16" : {
 			"price" : "17000",
-			"code" : "",
+			"code" : "KAD",
 		},
 		"5ce7dad5d999da000129db14" : {
 			"price" : "17000",
-			"code" : "",
+			"code" : "K23",
 		},
 		"5ce7dad5d999da000129db0c" : {
 			"price" : "17000",
-			"code" : "",
+			"code" : "G41",
 		},
 	},
-	"5dce6a51b7f58d0001bd7414" : {
+	"5dce6a51b7f58d0001bd7414" : { // model X-Trail
 		"5ce7dad6d999da000129db3d" : {
 			"price" : "18000",
-			"code" : "",
+			"code" : "RAQ",
 		},
 		"5ce7dad5d999da000129db25" : {
 			"price" : "0",
-			"code" : "",
+			"code" : "QM1",
 		},
 		"5ce7dad5d999da000129db22" : {
 			"price" : "21000",
-			"code" : "",
+			"code" : "QAB",
 		},
 		"5ce7dad5d999da000129db16" : {
 			"price" : "18000",
-			"code" : "",
+			"code" : "KAD",
 		},
 		"5ce7dad5d999da000129db14" : {
 			"price" : "18000",
-			"code" : "",
+			"code" : "K23",
 		},
 		"5ce7dad5d999da000129db0c" : {
 			"price" : "18000",
-			"code" : "",
+			"code" : "G41",
 		},
 		"5ce7dad6d999da000129db33" : {
 			"price" : "21000",
-			"code" : "",
+			"code" : "EBB",
 		},
 		"5ce7dad6d999da000129db30" : {
 			"price" : "18000",
-			"code" : "",
+			"code" : "EAN",
 		},
 	},
 }
